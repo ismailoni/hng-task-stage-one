efd50658781810d9abc3425dd516e716e5bba02b
Oni Ismail Modupe <ismailoni80@gmail.com>
2025-02-03 13:58:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+let targetColor = null;
+let score = 0;
+let life = 3;
+
+async function loadColors() {
+    const response = await fetch("colors.json");
+    const data = await response.json();
+    return data.colors;
+}
+
+async function getRandomColors() {
+    let colors = await loadColors();
+    let shuffled = colors.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, 6);
+}
+
+
+async function startNewGame(resetScore = false) {
+    if (resetScore) score = 0;
+    document.getElementById("score").innerText = score;
+    document.getElementById("life").innerText = "❤️".repeat(life);
+    document.getElementById("gameStatus").innerText = "Guess the correct color!";
+
+    let colors = await getRandomColors();
+    targetColor = colors[Math.floor(Math.random() * colors.length)];
+    document.getElementById("targetColor").style.backgroundColor = targetColor;
+
+    const colorOptionsDiv = document.getElementById("colorOptions");
+    colorOptionsDiv.innerHTML = "";
+    colors.forEach(color => {
+        const button = document.createElement("button");
+        button.className = "color-option fade-in";
+        button.style.backgroundColor = color;
+        button.onclick = () => handleGuess(color);
+        colorOptionsDiv.appendChild(button);
+    });
+}
+
+
+function handleGuess(selectedColor) {
+    if (selectedColor === targetColor) {
+        score++;
+        document.getElementById("gameStatus").innerText = "Correct! 🎉";
+        setTimeout(() => {
+            startNewGame();
+        }, 1000)
+    } else {
+        life = life - 1;
+        document.getElementById("gameStatus").innerText = "Wrong! Try again. ❌";
+        startNewGame();
+    }
+    document.getElementById("score").innerText = score;
+
+    if (life == 0) {
+        const popup = document.getElementById("popup");
+        popup.innerHTML = message();
+        popup.style.display = "block";
+        document.getElementById("restartButton").addEventListener("click", () => {
+            window.location.reload();
+        });
+        
+        function message() {
+            return `
+                <span>
+                    <h1>Game Over!! 😭</h1>
+                    <button id="restartButton" class="new-game-btn fade-in">Restart</button>
+                </span>
+            `;
+        }
+    
+        }
+}
+
+window.onload = startNewGame;
+
